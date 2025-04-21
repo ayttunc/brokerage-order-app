a570e0306380bceb17c81128bc10f98bcd2baa41
aytun <aytunc.matrac@hotmail.com.tr>
2025-04-22 02:16:58 +0300

    Swagger- UI Added.

---
+++ src/main/java/com/brokerage/orderapp/config/OpenApiConfig.java
@@ -0,0 +1,14 @@
+package com.brokerage.orderapp.config;
+
+import io.swagger.v3.oas.annotations.enums.SecuritySchemeType;
+import io.swagger.v3.oas.annotations.security.SecurityScheme;
+import org.springframework.context.annotation.Configuration;
+
+@SecurityScheme(
+        name = "bearerAuth",
+        type = SecuritySchemeType.HTTP,
+        scheme = "bearer",
+        bearerFormat = "JWT"
+)
+@Configuration
+public class OpenApiConfig { }
